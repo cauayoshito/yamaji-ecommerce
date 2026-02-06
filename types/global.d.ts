e1cf3e865764e90f017d404425c31a832c2d669e
cauayoshito <cauayoshito1@gmail.com>
2026-02-06 20:03:45 -0300

    Add plausible and fbq methods to Window interface

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,9 +1,10 @@
-// types/global.d.ts
 export {};
 
 declare global {
   interface Window {
+    plausible?: (eventName: string, options?: { props?: Record<string, any> }) => void;
     gtag?: (...args: any[]) => void;
+    fbq?: (...args: any[]) => void;
     dataLayer?: any[];
   }
 }
